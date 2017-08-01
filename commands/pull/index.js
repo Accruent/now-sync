baf24889c85cee615b3a10ab65acbc00611bb2ec
Kai Yao <kai.b.yao@gmail.com>
2017-08-01 13:15:45 -0500

    CORE-406 Push feature * `now push` will now copy all synced file content from local files to their respective records.
    CORE-407 Pull feature
    * `now pull` overwrites all synced local file content with fetched record data from ServiceNow.
    * CORE-369 `now sync` help text includes danger text.

---
+++ commands/pull/index.js
@@ -0,0 +1,18 @@
+const ora = require('ora');
+const CommandParser = require('../command-parser');
+const { pull } = require('../../util/sync');
+
+module.exports =
+class Pull extends CommandParser {
+  constructor(args) {
+    super(args);
+
+    this.requiresConfigFile = true;
+  }
+
+  async action() {
+    const spinner = ora('Pulling file content from ServiceNow...').start();
+    await pull();
+    spinner.stop();
+  }
+};
---
+++ commands/push/index.js
@@ -0,0 +1,18 @@
+const ora = require('ora');
+const CommandParser = require('../command-parser');
+const { push } = require('../../util/sync');
+
+module.exports =
+class Push extends CommandParser {
+  constructor(args) {
+    super(args);
+
+    this.requiresConfigFile = true;
+  }
+
+  async action() {
+    const spinner = ora('Pushing file content to ServiceNow...').start();
+    await push();
+    spinner.stop();
+  }
+};
