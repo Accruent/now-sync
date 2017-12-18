88914c1bcde26a5ed3c244a7f0ee35dec396af42
Kai Yao <kai.b.yao@gmail.com>
2017-12-18 15:24:39 -0600

    SYNC-14 `now check` now checks for missing or non-configured files/record entries.

--- commands/check/index.js
+++ commands/check/index.js
@@ -1,5 +1,6 @@
+const _ = require('lodash');
 const CommandParser = require('../command-parser');
-const { logError, logInfo } = require('../../util/logging');
+const { logError, logInfo, logWarn } = require('../../util/logging');
 const { check } = require('../../util/check');
 
 module.exports = class Config extends CommandParser {
@@ -13,7 +14,15 @@ module.exports = class Config extends CommandParser {
     let i;
 
     logInfo('Checking for duplicates…');
-    const { duplicates } = check(this.config);
+    const {
+      duplicates,
+      removedDuplicates,
+      missingFiles,
+      missingTableRecordEntries,
+      missingTableConfigs,
+      missingTableRecordObjects
+    } = check(this.config);
+
     for (i = 0; i < duplicates.length; i++) {
       const { code, data } = duplicates[i];
 
@@ -38,7 +47,7 @@ module.exports = class Config extends CommandParser {
 
           let j;
           for (j = 0; j < fileNames.length; j++) {
-            logError(`  ${fileNames[j]}`);
+            logInfo(`  ${fileNames[j]}`);
           }
           break;
         }
@@ -53,6 +62,58 @@ module.exports = class Config extends CommandParser {
     }
     if (!duplicates.length) {
       logInfo('No duplicate files or config entries found.');
+    }
+
+    const removedDuplicateTables = _.keys(removedDuplicates);
+    for (i = 0; i < removedDuplicateTables.length; i++) {
+      const table = removedDuplicateTables[i];
+      const duplicateTableFiles = removedDuplicates[table];
+
+      let j;
+      for (j = 0; j < duplicateTableFiles.length; j++) {
+        logInfo(
+          `Removed duplicate file entry for table ${table}: ${
+            duplicateTableFiles[j]
+          }`
+        );
+      }
+    }
+
+    logInfo('');
+    logInfo('Checking for missing files…');
+
+    if (missingTableConfigs.length) {
+      logWarn(
+        'Folders found that don’t have a matching config (check the `config` section of your .now-sync config):'
+      );
+      for (i = 0; i < missingTableConfigs.length; i++) {
+        logInfo(`  ${missingTableConfigs[i]}`);
+      }
+    }
+
+    if (missingTableRecordObjects.length) {
+      logWarn(
+        'Folders found that don’t have a matching record object (check the `records` section of your .now-sync config):'
+      );
+      for (i = 0; i < missingTableRecordObjects.length; i++) {
+        logInfo(`  ${missingTableRecordObjects[i]}`);
+      }
+    }
+
+    if (missingFiles.length) {
+      logWarn('Records entries that match non-existent files:');
+      for (i = 0; i < missingFiles.length; i++) {
+        logInfo(`  ${missingFiles[i]}`);
+      }
+    }
+
+    if (!_.isEmpty(missingTableRecordEntries)) {
+      logWarn(
+        'Files found that don’t have a matching now-sync record entry (use `now add` to add these record entries):'
+      );
+      for (i = 0; i < missingTableRecordEntries.length; i++) {
+        logInfo(`  ${missingTableRecordEntries[i]}`);
+      }
     }
   }
 };
--- util/check.js
+++ util/check.js
@@ -1,7 +1,10 @@
 const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
 const { getFieldValuesFromFileName } = require('./file-naming');
+const { saveConfigFile } = require('./config');
 
-const errorCodes = {
+const resultCodes = {
   FILE_CONFIG_NOT_FOUND: (table, contentField, fileName) => ({
     table,
     contentField,
@@ -14,19 +17,33 @@ const errorCodes = {
     sysId
   })
 };
-exports.errorCodes = errorCodes;
+exports.resultCodes = resultCodes;
 
 /**
  * Runs the entire suite of checks
  *
- * @param {any} config
- * @returns
+ * @param {object} config
+ * @returns {{duplicates: object[], removedDuplicates: Object.<string, string>}} check results
  */
 function check(config) {
   const duplicates = detectDuplicates(config);
+  const removedDuplicates = removeDuplicates(config, duplicates);
+  const {
+    missingFiles,
+    missingTableRecordEntries,
+    missingTableConfigs,
+    missingTableRecordObjects
+  } = detectMissingFiles(config);
+
+  saveConfigFile(config);
 
   return {
-    duplicates
+    duplicates,
+    removedDuplicates,
+    missingFiles,
+    missingTableRecordEntries,
+    missingTableConfigs,
+    missingTableRecordObjects
   };
 }
 exports.check = check;
@@ -72,7 +89,7 @@ function detectDuplicates(config) {
         } else {
           problems.push({
             code: 'FILE_CONFIG_NOT_FOUND',
-            data: errorCodes.FILE_CONFIG_NOT_FOUND(
+            data: resultCodes.FILE_CONFIG_NOT_FOUND(
               table,
               contentField,
               fileName
@@ -93,7 +110,7 @@ function detectDuplicates(config) {
         if (uniqueTableRecordsArr.length > 1) {
           problems.push({
             code: 'DUPLICATE_FILES',
-            data: errorCodes.DUPLICATE_FILES(
+            data: resultCodes.DUPLICATE_FILES(
               table,
               uniqueTableRecordsArr[0].contentField,
               uniqueTableRecordsArr[0].sysId,
@@ -108,6 +125,145 @@ function detectDuplicates(config) {
   return problems;
 }
 exports.detectDuplicates = detectDuplicates;
+
+/**
+ * Removes duplicate config record entries from the config object
+ *
+ * @param {object} config The config JSON object
+ * @param {{Array.<{code: string, data: object}>}} duplicates Array of duplicate objects
+ */
+function removeDuplicates(config, duplicates) {
+  const results = {};
+  const duplicatesArr = _.filter(
+    duplicates,
+    obj => obj.code === 'DUPLICATE_FILES'
+  );
+
+  let i;
+  for (i = 0; i < duplicatesArr.length; i++) {
+    const { fileNames, table } = duplicatesArr[i].data;
+    const fileNameCount = _.countBy(fileNames);
+    const uniqueFileNames = _.keys(fileNameCount);
+
+    let j;
+    for (j = 0; j < uniqueFileNames.length; j++) {
+      const fileName = uniqueFileNames[j];
+
+      if (fileNameCount[fileName] > 1) {
+        if (!results[table]) {
+          results[table] = [];
+        }
+
+        let k;
+        for (k = 0; k < fileNameCount[fileName] - 1; k++) {
+          const configLastRecordIndex = _.findLastIndex(
+            config.records[table],
+            record => record.fileName === fileName
+          );
+
+          results[table].push(fileName);
+          config.records[table] = [
+            ...config.records[table].slice(0, configLastRecordIndex),
+            ...config.records[table].slice(configLastRecordIndex + 1)
+          ];
+        }
+
+        results[table] = _.uniq(results[table]);
+      }
+    }
+  }
+
+  return results;
+}
+exports.removeDuplicates = removeDuplicates;
+
+/**
+ * Checks the now-sync config’s records to ensure all files exist, and checks the file folders for any files that don’t exist in the now-sync config.
+ *
+ * @param {object} config The config object
+ * @returns {{missingFiles: string[], missingTableRecordEntries: string[], missingTableConfigs: string[], missingTableRecordObjects: string[]}} All discrepancies found between the configured records and the folder/file structure
+ */
+function detectMissingFiles(config) {
+  const { config: nowConfig, records, filePath: configFilePath } = config;
+  const nowFilePath = path.resolve(process.cwd(), configFilePath);
+
+  // exists as a records entry but file not found
+  const missingFiles = [];
+
+  // files found that don’t exist as a records entry
+  const missingTableRecordEntries = [];
+
+  // folders found that don’t exist as a config key
+  const missingTableConfigs = [];
+
+  // folders found that don’t exist as a records key
+  const missingTableRecordObjects = [];
+
+  const tableFolders = fs.readdirSync(nowFilePath);
+
+  let i;
+  for (i = 0; i < tableFolders.length; i++) {
+    const tableFolder = tableFolders[i];
+    const tableFolderPath = path.resolve(nowFilePath, tableFolder);
+    const tableFolderPathRelative = path.join(configFilePath, tableFolder);
+    const stats = fs.statSync(tableFolderPath);
+
+    if (stats.isDirectory()) {
+      const isTableConfigFound = !!nowConfig[tableFolder];
+      const tableRecords = records[tableFolder];
+
+      // check for extra folders not found in now-sync config
+      if (!isTableConfigFound) {
+        missingTableConfigs.push(tableFolderPathRelative);
+      }
+
+      if (!tableRecords) {
+        missingTableRecordObjects.push(tableFolderPathRelative);
+      } else {
+        // for each folder, check for files not found in now-sync config records
+        const recordFiles = fs.readdirSync(tableFolderPath);
+        for (let j = 0; j < recordFiles.length; j++) {
+          const recordFileName = recordFiles[j];
+          const foundRecordEntry = _.find(
+            tableRecords,
+            ({ fileName }) => fileName === recordFileName
+          );
+          if (!foundRecordEntry) {
+            const filePathRelative = path.join(
+              tableFolderPathRelative,
+              recordFileName
+            );
+            missingTableRecordEntries.push(filePathRelative);
+          }
+        }
+
+        // check each config records entry to make sure that the file exists
+        _.forEach(tableRecords, ({ fileName }) => {
+          const filePath = path.resolve(tableFolderPath, fileName);
+          const filePathRelative = path.join(tableFolderPathRelative, fileName);
+          try {
+            const fileStats = fs.statSync(filePath);
+            if (!fileStats.isFile()) {
+              missingFiles.push(filePathRelative);
+            }
+          } catch (e) {
+            if (e.code === 'ENOENT') {
+              missingFiles.push(filePathRelative);
+            }
+          }
+        });
+      }
+    }
+  }
+
+  return {
+    missingFiles,
+    missingTableRecordEntries,
+    missingTableConfigs,
+    missingTableRecordObjects
+  };
+}
+exports.detectMissingFiles = detectMissingFiles;
 
 /**
  * Extracts the sys_id string from a given file name and file template and returns it.
